71bcf70bc256a5d0f696db7c1a12e27c040e0056
fajarfadillahh <fajaragustian979@gmail.com>
2022-03-03 16:44:23 +0700

    header section

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,16 @@ import "../css/index.css";
 // ===== Repo SwiperJS =====
 import Swiper from "swiper";
 import "swiper/css";
+
+// ===== SHOW MENU =====
+function showMenu(menuid, toggleid) {
+  const menu = document.getElementById(menuid);
+  const toggle = document.getElementById(toggleid);
+
+  if (menu && toggle) {
+    toggle.onclick = () => {
+      menu.classList.toggle("show-menu");
+    };
+  }
+}
+showMenu("header-menu", "header-toggle");
